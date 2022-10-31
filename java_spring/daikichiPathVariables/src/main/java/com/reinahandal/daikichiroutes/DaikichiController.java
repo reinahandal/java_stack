1fabd41783c5fc346b4df977d14de280ff050cb4
reinahandal <handalreina@gmail.com>
2022-10-31 14:09:36 +0200

    solution

---
+++ java_spring/daikichiPathVariables/src/main/java/com/reinahandal/daikichiroutes/DaikichiController.java
@@ -0,0 +1,36 @@
+package com.reinahandal.daikichiroutes;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daikichi")
+public class DaikichiController {
+	@RequestMapping("")
+	public String welcome() {
+		return "Welcome!";
+	}
+	@RequestMapping("/today")
+	public String today() {
+		return "Today you will find luck in all your endeavors!";
+	}
+	@RequestMapping("/tomorrow")
+	public String tomorrow() {
+		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city;
+	}
+	@RequestMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") String num) {
+		int x= Integer.parseInt(num);
+		if(x%2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
+}
